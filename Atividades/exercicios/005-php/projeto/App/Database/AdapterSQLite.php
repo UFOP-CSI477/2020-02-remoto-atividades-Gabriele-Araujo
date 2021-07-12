398f45ea85f4b7f9c7d908dc9a9833bb709578fb
Gabriele-Araujo <gabriele.araujo@aluno.ufop.edu.br>
2021-07-12 19:29:01 -0300

    Projeto php

---
+++ Atividades/exercicios/005-php/projeto/App/Database/AdapterSQLite.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+
+class AdapterSQLite implements AdapterInterface {
+
+    public function open() {
+
+    }
+
+    public function close() {
+        
+    }
+
+    public function get() {
+   
+    }
+
+}
