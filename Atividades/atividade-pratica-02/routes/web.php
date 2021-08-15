6a718fa8f2d309390920b95b3f6aa5847388cb76
Gabriele-Araujo <gabriele.araujo@aluno.ufop.edu.br>
2021-08-15 18:33:14 -0300

    Atividade02

--- Atividades/atividade-pratica-02/routes/web.php
+++ Atividades/atividade-pratica-02/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/administrativo', function () {
+    return view('administrativo');
+});
+
+Route::get('/suporte', function () {
+    return view('suporte');
+});
