31ea1c5432b2067979a63a66bb00c013a9cb6a88
Gabriele-Araujo <gabriele.araujo@aluno.ufop.edu.br>
2021-08-17 11:50:56 -0300

    Atividade02

--- Atividades/atividade-pratica-02/resources/views/principal.blade.php
+++ Atividades/atividade-pratica-02/resources/views/principal.blade.php
@@ -2,13 +2,16 @@
 
 @section('content')
 
-<h1 class="text-center">Tela Principal</h1>
+<h1 class="text-center">Sistema de Manutenção de Equipamentos</h1>
     <div class="text-center">
         <a href="{{route('equipamentos.index')}}">
             <button class="btn btn-primary">Área Administrativa</button>
         </a>
         <a href="{{route('suporte.index')}}">
             <button class="btn btn-primary">Área Geral - Suporte</button>
+        </a>
+        <a href="{{route('registros.index')}}">
+            <button class="btn btn-primary">Registros</button>
         </a>
     </div>
 @endsection
