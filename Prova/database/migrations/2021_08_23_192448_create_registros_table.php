81a230ece0b2651e259bcae253d9fbf7e8837bba
Gabriele-Araujo <gabriele.araujo@aluno.ufop.edu.br>
2021-08-23 16:54:08 -0300

    Prova

---
+++ Prova/database/migrations/2021_08_23_192448_create_registros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->increments('id')->key;
+            $table->foreign('pessoa_id')->references('id')->on('pessoas');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+            $table->foreign('vacina_id')->references('id')->on('vacinas');
+            $table->integer('dose');
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
